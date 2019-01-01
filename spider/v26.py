f5e2f842e4765bf6be3798e638b75deaeddacf56
Fang-Joker <593367432@qq.com>
2019-01-01 12:31:33 +0800

    19.1.1 finished

---
+++ spider/v26.py
@@ -0,0 +1,16 @@
+'''
+findall案例
+'''
+
+import re
+
+pattern = re.compile(r'\d+')
+s = pattern.findall("i am 18 years odl and 185 high")
+print(type(s))
+print(s)
+
+s = pattern.finditer("i am 18 years odl and 185 high")
+print(type(s))
+
+for i in s:
+    print(i.group())
